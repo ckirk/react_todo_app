52cd2f5550a906b43ef65143ff302777d882d093
Chris <ckirkinis@gmail.com>
2016-10-18 18:15:10 -0400

    Revert "reading list from json file on server, saving new items to server"
    This reverts commit dab076c41b0942877251d434ebb06424ebc500bb.

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -1,64 +1,24 @@
-// var data = [  
-//   {key: 0, text: 'Buy milk', status: 'completed'}, 
-//   {key: 1, text: 'Call Caron back', status: ''},
-//   {key: 2, text: 'Mow the lawn', status: ''} 
-// ];
+var data = [  
+  {key: 0, text: 'Buy milk', status: 'completed'}, 
+  {key: 1, text: 'Call Sharon back', status: ''},
+  {key: 2, text: 'Mow the lawn', status: ''} 
+];
+
+var test = 'test';
 
 var TodoContainer = React.createClass({
   getInitialState: function() {
-    return { text: '', data: []};
+    return { text: '', data: data };
   },
-  // fired when component first loads (only 1x)
-  componentDidMount: function() {
-    this.getTodosFromServer();
-    // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-  getTodosFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleTodoSubmit: function(text) {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: text,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  // fired on form submit
   addTodoItem: function(e) {
     e.preventDefault();
 
     // push to list
-    this.handleTodoSubmit({text: this.state.text, status: ''});
-    console.log(this.state.text);
-    // var nextKey = this.state.data.length + 1;
-    // this.state.data.push({key: nextKey, text: this.state.text});
+    var nextKey = this.state.data.length + 1;
+    this.state.data.push({key: nextKey, text: this.state.text});
 
     // clear input box
     this.setState({text: ''});
-  },
-  toggleCompleted: function(id) {
-    if (this.state.status === 'completed') {
-      this.setState({status: 'incomplete'});
-    } else {
-      this.setState({status: 'completed'});
-    }
   },
   removeTodoItem: function(index) {
     this.state.data.splice(index, 1);
@@ -66,14 +26,13 @@ var TodoContainer = React.createClass({
     // set state to update
     this.setState({data: this.state.data});
   },
-  // triggered with each keystroke
   handleItemChange: function(e) {
     this.setState({text: e.target.value});
   },
   render: function() {
     var todoItems = this.state.data.map(function(object, i) {
       return (
-        <TodoItem text={object.text} status={object.status} delete={this.removeTodoItem} toggleCompleted={this.toggleCompleted} key={object.id} index={i} id={object.id} />
+        <TodoItem text={object.text} status={object.status} delete={this.removeTodoItem} key={object.key} index={i} />
       );
     }, this);
     return (
@@ -101,9 +60,16 @@ var TodoItem = React.createClass({
   getInitialState: function() {
     return { status: this.props.status };
   },
+  toggleCompleted: function() {
+    if (this.state.status === 'completed') {
+      this.setState({status: 'incomplete'});
+    } else {
+      this.setState({status: 'completed'});
+    }    
+  },
   render: function() {
     return (
-      <li className={this.state.status + ' noselect todoItem'} onClick={this.toggleCompleted.bind(null, this.props.id)}>
+      <li className={this.state.status + ' noselect todoItem'} onClick={this.toggleCompleted}>
         {this.props.text}
         <div className="right" onClick={this.props.delete.bind(null, this.props.index)}>delete</div>
       </li>
@@ -112,6 +78,6 @@ var TodoItem = React.createClass({
 });
 
 ReactDOM.render(
-  <TodoContainer url="/api/todos" />,
+  <TodoContainer data={data} />,
   document.getElementById('content')
 );
